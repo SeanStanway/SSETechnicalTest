dde0b919b04c492404ebe1d710b1df1abb655eb2
Sean Stanway <sean.stanway@oneadvanced.com>
2021-01-19 14:06:49 +0000

    Add project files.

---
+++ SSETechnicalTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSETechnicalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
